092a67abc28af5686f9eab59399d4d08c93484dc
DIR\vinnel.lazaro <vinnel.lazaro@accenture.com>
2022-10-07 16:18:21 +0800

    Dashboard Design

---
+++ js/main.js
@@ -0,0 +1,34 @@
+const labels = [
+    'January',
+    'February',
+    'March',
+    'April',
+    'May',
+    'June',
+  ];
+
+  const data = {
+    labels: labels,
+    datasets: [{
+      label: 'New',
+      backgroundColor: 'rgb(255, 99, 132)',
+      borderColor: 'rgb(255, 99, 132)',
+      data: [0, 10, 5, 2, 20, 30, 45],
+    },{
+        label: 'Renewal',
+        backgroundColor: 'rgb(255, 99, 132)',
+        borderColor: 'rgb(255, 99, 132)',
+        data: [5, 10, 15, 5, 20, 40, 55],
+      }]
+  };
+
+  const config = {
+    type: 'line',
+    data: data,
+    options: {}
+  };
+
+  const myChart = new Chart(
+    document.getElementById('myChart'),
+    config
+  );
